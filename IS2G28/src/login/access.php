33257036b83cec0aff192c5e777581007e62e340
Juan Cipollone <jmcipollone@gmail.com>
2017-05-31 08:15:46 -0300

    Configuracion de composer para que el autoload detecte las nuevas clases del modelo de forma automatica. Es necesario hacer un composer update para que tengan efecto los cambios.

--- IS2G28/src/login/access.php
+++ IS2G28/src/login/access.php
@@ -2,7 +2,7 @@
 require '../../config/doctrine_config.php';
 if (isset($_POST)&&($_POST['user']!=null)&&($_POST['password']!=null)) {
 	if (!filter_var($_POST['user'], FILTER_VALIDATE_EMAIL)=== false) {
-		$user = $entityManager->getRepository('MyDomain\Model\User')->findBy(array('mail'=>$_POST['user']));
+		$user = $entityManager->getRepository('User')->findBy(array('mail'=>$_POST['user']));
 		var_dump($user);
 		var_dump($_POST['user']);
 	} else {
