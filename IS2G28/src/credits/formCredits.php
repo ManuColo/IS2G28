ce1f17dfb2baa44a6ff24d938d3088d2fa21fc96
superusuario <superusuario@SuperMaquina>
2017-05-31 02:52:27 -0300

    Formulario compra de créditos

--- IS2G28/src/credits/formCredits.php
+++ IS2G28/src/credits/formCredits.php
@@ -17,50 +17,50 @@
 			</div>
         <div class="panel-body">
         <!-- Formulario de solicitud de crédito -->
-          <form class="form-horizontal">
+          <form class="form-horizontal" method="post">
 			<!-- Tipo de tarjeta -->
 			<div class="form-group">
-			  <label for="user" class="col-sm-2 control-label">Tarjeta</label>
+			  <label for="card" class="col-sm-2 control-label">Tarjeta</label>
 				<div class="col-sm-10">
-					<select class="selectpicker">
-						<option> </option>
-						<option>VISA Débito</option>
-						<option>VISA Crédito</option>
+					<select class="selectpicker" id="card" name="card">
+						<option id="0"> </option>
+						<option id="vd">VISA Débito</option>
+						<option id="vc">VISA Crédito</option>
 					</select>
 			  	</div>
 			</div>
             <!-- Banco -->
             <div class="form-group">
-			  <label for="pass" class="col-sm-2 control-label">Banco</label>
+			  <label for="bank" class="col-sm-2 control-label">Banco</label>
 				<div class="col-sm-10">
-                    <select class="selectpicker">
-						<option> </option>
-						<option>Provincia</option>
-						<option>Nación</option>
+                    <select class="selectpicker" id="bank" name="bank">
+						<option id="0"> </option>
+						<option id="bp">Provincia</option>
+						<option id="bn">Nación</option>
 					</select>
                 </div>
             </div>
             <!-- Número -->
             <div class="form-group">
-			  <label for="user" class="col-sm-2 control-label">Número</label>
+			  <label for="numCard" class="col-sm-2 control-label">Número</label>
               	<div class="col-sm-10">
-					<input type="number" class="form-control" id="user" />
+					<input type="text" class="form-control" id="numCard" name="numCard" placeholder="Sólo números" />
                 </div>
             </div>
             <!-- Código de seguridad -->
             <div class="form-group">
-			  <label for="user" class="col-sm-2 control-label">Código</label>
+			  <label for="codCard" class="col-sm-2 control-label">Código</label>
 				<div class="col-sm-10">
-					<input type="number" class="form-control" id="user" />
+					<input type="text" class="form-control" id="codCard" name="codCard" placeholder="Tres dígitos" />
                 </div>
             </div>
             <!-- Cantidad de créditos -->
             <div class="form-group">
-			  <label for="user" class="col-sm-2 control-label">Cantidad</label>
+			  <label for="cantCred" class="col-sm-2 control-label">Cantidad</label>
 				<div class="col-sm-10">
 					<select class="selectpicker">
-						<option> - </option>
-  						<option> 1 </option>
+						<option id="0"> - </option>
+  						<option id="1"> 1 </option>
 					</select>
                 </div>
             </div>
@@ -70,7 +70,7 @@
                 <div class="col-sm-10 col-sm-offset-1">                                  
                   <input type="submit" class="btn btn-primary" value="Comprar">
                   <input type="reset" class="btn btn-primary" value="Reiniciar">
-                  <a type="button" class="btn btn-primary" href="../login/index.php">Inicio</a>
+                  <a type="button" class="btn btn-primary" onClick="goBack();">Cancelar</a>
 				</div>
 			</div>              
           </form>
