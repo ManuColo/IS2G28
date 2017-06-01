932dd124e4a2f9528d6880590e4f53f88ef11ac6
manuel <manuel@deb8.rec.utn.edu.ar>
2017-06-01 12:14:20 -0300

    Clase Credit

---
+++ IS2G28/src/model/Credits.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Description of User
+ *
+ * @author manuel
+ * @Entity @Table(name="credits")
+ */
+
+Class Credit {
+	/**
+	 * @Id @Column(type="integer") @GeneratedValue
+	 * @var integer
+	 */
+	private $id;
+	
+	/**
+	 * @Column(type="date")
+	 * @var DateTime 
+	 */
+	private $operationDate;
+	
+	/**
+	 * @Column(type="integer")
+	 * @var integer
+	 */
+	private $cantidad;
+	
+	/**
+	 * @Column(type="integer")
+	 * @var integer
+	 */
+	private $userId;
+	
+	public function setId($id){
+		$this->id = $id;
+	}
+	
+	public function setOperationDate($date){
+		$this->operationDate= $date;
+	}
+	
+	public function setCantidad($cantidad){
+		$this->cantidad = $cantidad;
+	}
+	
+	public function setUserId($id){
+		$this->userId = $id;
+	}
+	
+	public function getId(){
+		return $this->id;
+	}
+	
+	public function getOperationDate(){
+		return $this->operationDate;
+	}
+	
+	public function getCantidad(){
+		return $this->cantidad;
+	}
+	
+	public function getUserId(){
+		return $this->userId;
+	}
+
+}
