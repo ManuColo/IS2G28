0d4bd0615384d4103d27a144fd1c35f05dd36752
superusuario <superusuario@SuperMaquina>
2017-06-01 02:43:43 -0300

    Corrección definiciones

--- IS2G28/src/registro/registro.php
+++ IS2G28/src/registro/registro.php
@@ -3,16 +3,6 @@
   <head>
 	<title>Una Gauchada - Registro de usuarios</title>
 	<?php require '../common/common_headers.php' ;?>
-	<script type="text/javascript">
-		$(document).ready(function() {
-			$('#login-form').on('submit', function() {
-			var user = $('#user').val();
-			if (!isEmail(user)) {
-				return false;
-				}
-			});
-		});
-	</script>
   </head>
 	<body>
       <!-- Contenedor principal, requerido por Bootstrap -->
@@ -30,37 +20,37 @@
           <form class="form-horizontal" method="post" action="signin.php">
 			<!-- Nombre del usuario -->
 			<div class="form-group">
-			  <label for="name" class="col-sm-2 control-label" onchange="validarN('name')">Nombre</label>
+			  <label for="name" class="col-sm-2 control-label">Nombre</label>
 				<div class="col-sm-10">
-					<input type="text" class="form-control" id="name" name="name" />
+					<input type="text" class="form-control" id="name" name="name" required />
 			  	</div>
 			</div>
             <!-- Apellido del usuario -->
             <div class="form-group">
 			  <label for="lastname" class="col-sm-2 control-label">Apellido</label>
 				<div class="col-sm-10">
-                    <input type="text" class="form-control" id="lastname" name="lastname" />
+                    <input type="text" class="form-control" id="lastname" name="lastname" required />
                 </div>
             </div>
             <!-- Email del usuario -->
             <div class="form-group">
 			  <label for="mail" class="col-sm-2 control-label">E-mail</label>
               	<div class="col-sm-10">
-					<input type="email" class="form-control" id="mail" name="mail" />
+					<input type="email" class="form-control" id="mail" name="mail" required />
                 </div>
             </div>
             <!-- Teléfono del usuario -->
             <div class="form-group">
 			  <label for="phone" class="col-sm-2 control-label">Teléfono</label>
 				<div class="col-sm-10">
-					<input type="tel" class="form-control" id="phone" name="phone" />
+					<input type="tel" class="form-control" id="phone" name="phone" required />
                 </div>
             </div>
             <!-- Contraseña del usuario -->
             <div class="form-group">
 			  <label for="password" class="col-sm-2 control-label">Clave</label>
 				<div class="col-sm-10">
-					<input type="password" class="form-control" id="password" name="password" />
+					<input type="password" class="form-control" id="password" name="password" required />
                 </div>
             </div>
              
