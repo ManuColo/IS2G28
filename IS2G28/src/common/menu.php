d58e3749c8be3b8d2a6a57cd9e0a5bcc8173bc9d
manuel <manuel@deb8.rec.utn.edu.ar>
2017-06-01 14:31:16 -0300

    Agregado de height al nav agragado de tag nav

--- IS2G28/src/common/menu.php
+++ IS2G28/src/common/menu.php
@@ -1,15 +1,17 @@
-<ul class="nav nav-pills pull-right">
-<?php
-if (isset($_SESSION['logged'])) {
-	if($_SESSION['logged']== true){ ?>
+<nav>
+	<ul class="nav nav-pills pull-right">
+	<?php
+	if (isset($_SESSION['logged'])) {
+		if($_SESSION['logged']== true){ ?>
+			<li class="active"><a href="<?php echo $cfg->wwwRoot;?>/src">Inicio</a></li>
+			<li><a href="<?php echo $cfg->wwwRoot;?>/src/favors/list.php">Favores</a></li>
+			<li><a href="<?php echo $cfg->wwwRoot;?>/src/credits/formCredits.php">Cr&eacute;ditos</a></li>
+			<li><a href="<?php echo $cfg->wwwRoot;?>/src/login/logout.php">Cerrar Sesión</a></li>
+		<?php } 
+	} else { ?>
 		<li class="active"><a href="<?php echo $cfg->wwwRoot;?>/src">Inicio</a></li>
-		<li><a href="<?php echo $cfg->wwwRoot;?>/src/favors/list.php">Favores</a></li>
-		<li><a href="<?php echo $cfg->wwwRoot;?>/src/credits/formCredits.php">Cr&eacute;ditos</a></li>
-		<li><a href="<?php echo $cfg->wwwRoot;?>/src/login/logout.php">Cerrar Sesión</a></li>
-	<?php } 
-} else { ?>
-	<li class="active"><a href="<?php echo $cfg->wwwRoot;?>/src">Inicio</a></li>
-	<li><a href="<?php echo $cfg->wwwRoot;?>/src/login/login.php">Iniciar Sesión</a></li>
-	<li><a href="<?php echo $cfg->wwwRoot;?>/src/registro/registro.php">Registrate</a></li>
-<?php }?>
-</ul>
+		<li><a href="<?php echo $cfg->wwwRoot;?>/src/login/login.php">Iniciar Sesión</a></li>
+		<li><a href="<?php echo $cfg->wwwRoot;?>/src/registro/registro.php">Registrate</a></li>
+	<?php }?>
+	</ul>
+</nav>
