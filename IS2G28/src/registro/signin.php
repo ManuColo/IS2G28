8a47ff2c8a0d4ef81c2b6d3d9de96ba7bbaa7a33
superusuario <superusuario@SuperMaquina>
2017-06-01 02:45:13 -0300

    Verifica que no inserte usuario duplicado en bbdd. Mensajes de operación

--- IS2G28/src/registro/signin.php
+++ IS2G28/src/registro/signin.php
@@ -11,22 +11,33 @@ if (isset($_POST['name'])&&
 		($_POST['phone']!=null)&&
 		($_POST['password']!=null)
 	){
-	$nom=$_POST['name'];
-	$ape=$_POST['lastname'];
-	$mail=$_POST['mail'];
-	$tel=$_POST['phone'];
-	$cla=$_POST['password']; 
-	$user=new User() ;
-	$user->setName ($nom);
-	$user->setLastname($ape);
-	$user->setMail($mail);
-	$user->setPhone($tel);
-	$user->setSalt($user->generateSalt());
-	$user->setPass($user->encryptPassword($cla,$user->getSalt())); 
-	$entityManager-> persist($user);
-	$entityManager-> flush();
-	header("location:../login/login.php?message=userCreated");
-} else {
-	header("location:registro.php");
+		//Verifico si el mail existe en la base de datos
+		$compare = $entityManager->getRepository('User')->findBy(array('mail'=>$_POST['mail']))[0];
+		if($compare == null){
+			$nom=$_POST['name'];
+			$ape=$_POST['lastname'];
+			$mail=$_POST['mail'];
+			$tel=$_POST['phone'];
+			$cla=$_POST['password']; 
+			$user=new User() ;
+			$user->setName ($nom);
+			$user->setLastname($ape);
+			$user->setMail($mail);
+			$user->setPhone($tel);
+			$user->setSalt($user->generateSalt());
+			$user->setPass($user->encryptPassword($cla,$user->getSalt())); 
+			$entityManager-> persist($user);
+			$entityManager-> flush();
+			//$_SESSION['error']= "Inicia Sesion para continuar";
+			header("location:../login/login.php?message=userCreated");
+		} 
+			else {
+			//$_SESSION['error']= "Tu e-mail ya existe en nuestra base de datos";
+			header("location:registro.php?message=userExists");
+		} 
+	} 
+	else {
+		//$_SESSION['error']= "Información enviada incompleta";
+		header("location:registro.php?message=notComplete");
 }
 ?>
