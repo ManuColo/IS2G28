8f3d005621e8ee54e4bb28020c3bd968daea5523
LaleCo <superusuario@deb8.rec.utn.edu.ar>
2017-06-09 01:55:10 -0300

    Agrego redirección si no tiene sesión iniciada

--- IS2G28/src/login/logout.php
+++ IS2G28/src/login/logout.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
-unset($_SESSION['logged']);
-unset($_SESSION['loginTime']);
-unset($_SESSION['userId']);
-session_destroy();
-header("location:../");
+if ($_SESSION['logged']) {
+	unset($_SESSION['logged']);
+	unset($_SESSION['loginTime']);
+	unset($_SESSION['userId']);
+	session_destroy();
+	header("location:../");
+} else {
+	header("location:../login/login.php");
+	exit();
+}
+?>
