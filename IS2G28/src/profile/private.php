fdba70006ecb08b42b7bf92024a6be61bf6ae1fa
manuel <manuel@deb8.rec.utn.edu.ar>
2017-06-30 20:51:24 -0300

    Separación de menú del perfil

--- IS2G28/src/profile/private.php
+++ IS2G28/src/profile/private.php
@@ -21,21 +21,7 @@ if ($_SESSION['logged']) { ?>
 	?> 
     <div class="jumbotron" id="profileJumb">
 		<!-- Menú del usuario -->
-		<nav class="btn-group-vertical" id="menuPP" role="group">
-				<Ul class="nav nav-stacked dropdown-menu-left">                                  
-            		<li id="menuEnc" class="nav-header"><p><strong>Mi Perfil</strong></p></li>
-            		<li class="nav-header"><a href="../registro/editRegForm.php">Editar Perfil<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
-            		<li class="nav-header"><strong>Gauchadas</strong></li>
-            		<li class="nav-header disabled"><a href="#">Mis Gauchadas<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
-            		<li class="nav-header disabled"><a href="#">Mis Postulaciones<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
-            	    <li class="nav-header"><strong>Calificaciones</strong></li>
-            	    <li class="nav-header disabled"><a href="#">Mis calificaciones<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
-					<li class="nav-header"><strong>Comentarios</strong></li>
-					<li class="nav-header disabled"><a href="#">En mis gauchadas pedidas<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
-					<li class="nav-header disabled"><a href="#">En gauchadas que hice<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
-				</Ul>
-		
-		</nav>
+		<?php include '../common/sidebar.php'; ?>
 		<div class="imgUsProf">
 			<?php if ($user->getPhoto()){ ?>
 					<img class="media-object img-circle userPhoto" src="../uploads/<?php echo $user->getPhoto() ?>" 
