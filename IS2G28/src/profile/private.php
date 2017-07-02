953c7333dc6f397a1dda459e216ebfc33a4cd910
LaleCo <superusuario@deb8.rec.utn.edu.ar>
2017-07-02 01:01:19 -0300

    Correcciones en menú

--- IS2G28/src/profile/private.php
+++ IS2G28/src/profile/private.php
@@ -21,11 +21,10 @@ if ($_SESSION['logged']) { ?>
 	?> 
     <div class="jumbotron" id="profileJumb">
 		<!-- Menú del usuario -->
-		<nav class="myml-menu-navigation menu-expandable" role="navigation">
-			<div class="btn-group-vertical" id="menuPP" role="group">
-				<Ul class="nav nav-tabs nav-stacked dropdown-menu-left">                                  
-            		<li id="menuEnc" class="nav-header"><p style="margin-bottom: 0px;">Mi Perfil</p></li>
-            		<li class="nav-header"><a href="../registro/editRegForm.php">Editar Perfil</a></li>
+		<nav class="btn-group-vertical" id="menuPP" role="group">
+				<Ul class="nav nav-stacked dropdown-menu-left">                                  
+            		<li id="menuEnc" class="nav-header"><p><strong>Mi Perfil</strong></p></li>
+            		<li class="nav-header"><a href="../registro/editRegForm.php">Editar Perfil<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
             		<li class="nav-header"><strong>Gauchadas</strong></li>
             		<li class="nav-header disabled"><a href="#">Mis Gauchadas<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
             		<li class="nav-header disabled"><a href="#">Mis Postulaciones<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
@@ -35,7 +34,7 @@ if ($_SESSION['logged']) { ?>
 					<li class="nav-header disabled"><a href="#">En mis gauchadas pedidas<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
 					<li class="nav-header disabled"><a href="#">En gauchadas que hice<b class="glyphicon glyphicon-menu-right" aria-hidden="true"></b></a></li>
 				</Ul>
-			</div>
+		
 		</nav>
 		<div class="imgUsProf">
 			<?php if ($user->getPhoto()){ ?>
