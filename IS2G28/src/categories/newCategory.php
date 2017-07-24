a5c800f0022e66b54385856e12e462711040c648
ManuColo <manuelcolombini@gmail.com>
2017-07-24 12:20:45 -0300

    Update newCategory.php

--- IS2G28/src/categories/newCategory.php
+++ IS2G28/src/categories/newCategory.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (!isset($_SESSION)) {
+	session_start();
+}
 require_once __DIR__.'/../../config/doctrine_config.php';
 $user= $entityManager->find('User',$_SESSION['userId']);
 if ($_SESSION['logged']) {
