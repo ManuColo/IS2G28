2d689a8e3783c1652b40492f75d705b16578b792
Juan Cipollone <jmcipollone@gmail.com>
2017-07-26 20:39:44 -0300

    Reputaciones: Definición inicial de la clase que modela las reputaciones.

---
+++ IS2G28/src/model/Reputation.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Clase que modela reputación de un usuario.
+ * 
+ * La reputación describe la categoría de un usuario según su puntaje.
+ *
+ * @author juan
+ * @Entity @Table(name="reputations")
+ */
+class Reputation 
+{
+  /**
+   * Identificador unívoco de la reputación.
+   * 
+   * @var integer
+   * @Id
+   * @GeneratedValue
+   * @Column(type="integer")
+   */
+  private $id;
+  
+  /**
+   * Nombre de la reputación.
+   * 
+   * @var string
+   * @Column(type="string", length=30)
+   */
+  private $name;
+  
+  /**
+   * Nombre de la imagen asociada a la reputación.
+   * 
+   * @var string 
+   * @Column(type="string")
+   */
+  private $image;
+  
+  /**
+   * Puntaje mínimo de la reputación.
+   * 
+   * @var integer
+   * @Column(type="integer" )
+   */
+  private $minScore;
+}
