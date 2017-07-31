704cb5b800cb044906ccd46dc745b158057129a2
Juan Cipollone <jmcipollone@gmail.com>
2017-07-31 11:09:12 -0300

    Creación de una reputación: Se agregaron validaciones en el procesamiento del formulario de alta de una reputación.

--- IS2G28/src/reputations/create.php
+++ IS2G28/src/reputations/create.php
@@ -6,6 +6,7 @@
  * @author Juan
  */
 
+use Symfony\Component\Validator\Validation;
 require_once __DIR__.'/../../config/doctrine_config.php';
 require_once __DIR__.'/../common/lib.php';
 
@@ -33,21 +34,60 @@ if (!$user->getIsAdmin()) {
 $reputationData = getRequestData($_POST['reputation'], $_FILES);
 $reputation = createReputation($reputationData);
 
-// Mover la imagen asociada a la reputación al directorio de uploads
-// Actualizar objeto reputación con la ubicación definitiva de la imagen
-$imageFileName = time() . basename($_FILES['reputation_image']['name']);
-$targetFile = $cfg->uploadDir . $imageFileName;
-move_uploaded_file($reputation->getImage(), $targetFile);    
-$reputation->setImage($imageFileName);
+// Validar datos de la nueva reputación
+$validator = Validation::createValidatorBuilder()
+  ->addMethodMapping('loadValidatorMetadata')
+  ->getValidator()
+;
+$violations = $validator->validate($reputation);
+$errors = array();
+foreach ($violations as $violation) {
+  $errors[$violation->getPropertyPath()] = $violation->getMessage();  
+}
 
-// Persistir nueva reputación en la base de datos
-$entityManager->persist($reputation);
-$entityManager->flush();
+if (count($violations) == 0) {
+  // Validar que no exista una reputación con el nombre especificado
+  $anotherReputation = $entityManager->getRepository('Reputation')->findByName($reputation->getName());
+  if ($anotherReputation) {
+    $errors['name'] = 'El nombre elegido corresponde a otra reputación.';
+  }  
+  // Validar que no exista una reputación con el puntaje mínimo especificado
+  $reputation2 = $entityManager->getRepository('Reputation')->findByMinScore($reputation->getMinScore());
+  if ($reputation2) {
+    $errors['minScore'] = 'El puntaje elegido corresponde a otra reputación.';
+  }  
+}
 
-// Redirigir al usuario a la pantalla de alta nuevamente
-header("location:new.php");
+
+
+if (count($errors) == 0) {
+  // Mover la imagen asociada a la reputación al directorio de uploads
+  // Actualizar objeto reputación con la ubicación definitiva de la imagen
+  $imageFileName = time() . basename($_FILES['reputation_image']['name']);
+  $targetFile = $cfg->uploadDir . $imageFileName;
+  move_uploaded_file($reputation->getImage(), $targetFile);    
+  $reputation->setImage($imageFileName);
+
+  // Persistir nueva reputación en la base de datos
+  $entityManager->persist($reputation);
+  $entityManager->flush();
+
+  // Redirigir al usuario a la pantalla de alta nuevamente
+  header("location:new.php");  
+}
+
+// Imprimir errores de validacion
+if (count($violations) > 0) {
+  $errorsString = (string) $violations;
+  echo $errorsString;
+}
+
+if (count($errors) > 0) {
+  echo '<br>';
+  var_dump($errors);
+}
+
  
-
 
 // ===============================================================================
 // Funciones privadas
@@ -83,7 +123,12 @@ function createReputation($reputationData)
 {
   $reputation = new Reputation();
   $reputation->setName($reputationData['name']);
-  $reputation->setMinScore($reputationData['minScore']);
+  // Convertir el puntaje mínimo en un valor numérico si corresponde
+  $minScore = $reputationData['minScore'];
+  if (filter_var($minScore, FILTER_VALIDATE_INT) !== false) {
+    $minScore = intval($minScore);
+  }    
+  $reputation->setMinScore($minScore);
   $reputation->setImage($reputationData['image']);
       
   return $reputation;
